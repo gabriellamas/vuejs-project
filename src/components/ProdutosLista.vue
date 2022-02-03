67f0050b2b5d3306b0ac628d68c3f1a67694b9d1
Gabriel Lamas <gabrielldarts@gmail.com>
2022-02-03 09:18:43 -0300

    add axios

--- src/components/ProdutosLista.vue
+++ src/components/ProdutosLista.vue
@@ -1,9 +1,37 @@
 <template>
-  <h2>Lista</h2>
+  <section class="container-products">
+    <div v-for="product in products" :key="product.id">
+      <img
+        v-if="product.fotos"
+        :src="product.fotos[0].src"
+        :alt="product.fotos[0].titulo"
+      />
+      <p>{{ product.preco }}</p>
+      <h2>{{ product.nome }}</h2>
+      <p>{{ product.descricao }}</p>
+    </div>
+  </section>
 </template>
 
 <script>
-export default {};
+import { api } from "@/services";
+export default {
+  data() {
+    return {
+      products: null,
+    };
+  },
+  methods: {
+    getProducts() {
+      api.get("/produto").then((response) => {
+        this.products = response.data;
+      });
+    },
+  },
+  created() {
+    this.getProducts();
+  },
+};
 </script>
 
 <style></style>
