0c9115d40650ea19b4f0ce634b1df6af6950e6e2
Gabriel Lamas <gabrielldarts@gmail.com>
2022-02-16 16:11:21 -0300

    get usuario produtos

---
+++ src/components/ProdutoAdicionar.vue
@@ -0,0 +1,11 @@
+<template>
+  <p>Adicionar produto</p>
+</template>
+
+<script>
+export default {
+  name: "ProdutoAdicionar",
+};
+</script>
+
+<style scoped></style>
--- src/views/usuario/UsuarioProdutos.vue
+++ src/views/usuario/UsuarioProdutos.vue
@@ -1,12 +1,35 @@
 <template>
   <section class="usuario-produtos">
-    <p>Produtos lista</p>
+    <h2>Adicionar produto</h2>
+    <ProdutoAdicionar />
+    {{ usuario_produtos }}
   </section>
 </template>
 
 <script>
+import ProdutoAdicionar from "@/components/ProdutoAdicionar.vue";
+import { mapState, mapActions } from "vuex";
 export default {
   name: "UsuarioProdutos",
+  components: {
+    ProdutoAdicionar,
+  },
+  computed: {
+    ...mapState(["login", "usuario", "usuario_produtos"]),
+  },
+  methods: {
+    ...mapActions(["getUsuarioProdutos"]),
+  },
+  created() {
+    if (this.login) {
+      this.getUsuarioProdutos();
+    }
+  },
+  watch: {
+    login() {
+      this.getUsuarioProdutos();
+    },
+  },
 };
 </script>
 
